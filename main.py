a2cfdb65e87301f9e948f6159d4d14e709031bda
Pau Pons <98814765+P2101@users.noreply.github.com>
2023-10-11 23:27:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,7 @@ for i in rands:
         first_r.append(equipos[i])
     else:
         second_r.append(equipos[i])
-print(first_r)
-print(second_r)
+
 
 def print_board(board):
     for row in board:
@@ -40,46 +39,44 @@ def check_win(board, player):
 
     return False
 
+def check_player():
+    name = input('Introduce el nombre del jugador:  ')
+    teams = set(df[df['player_name'] == name.title()]['team_abbreviation'])
+    print(teams)
+    return teams
+    
 def play_tic_tac_toe():
     board = [[" " for _ in range(3)] for _ in range(3)]
     current_player = "X"
 
     while True:
+        print(first_r)
+        print(second_r)
         print_board(board)
-        name = input('Introduce el nombre del jugador:  ')
-        teams = set(df[df['player_name'] == name.title()]['team_abbreviation'])
-        print(teams)
+        teams = check_player()
+        
         row = int(input(f"Player {current_player}, enter row (0, 1, 2): "))
         col = int(input(f"Player {current_player}, enter column (0, 1, 2): "))
-        for team in teams:
             
-            if first_r[row] in teams and second_r[col] in teams:
-                print('correcto')
-                break
-                # return True
-            else:
-                print('incorrecto')
-                break
-                # return False
-        # row = int(input(f"Player {current_player}, enter row (0, 1, 2): "))
-        # col = int(input(f"Player {current_player}, enter column (0, 1, 2): "))
-
         if board[row][col] != " ":
             print("Cell already taken. Try again.")
             continue
 
-        board[row][col] = current_player
-        if check_win(board, current_player):
-            print_board(board)
-            print(f"Player {current_player} wins!")
-            break
-
         if all(board[i][j] != " " for i in range(3) for j in range(3)):
             print_board(board)
             print("It's a draw!")
-            break
-
+            break     
+        
+        if first_r[row] in teams and second_r[col] in teams:
+            board[row][col] = current_player
+            if check_win(board, current_player):
+                print_board(board)
+                print(f"Player {current_player} wins!")
+                break
+        else:
+            print('Datos incorrectos')
         current_player = "O" if current_player == "X" else "X"
+
 
 if __name__ == "__main__":
     play_tic_tac_toe()
