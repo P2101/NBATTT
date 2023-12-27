d4434e11db59616c3368f6b09dd1d800f2f37d91
Pau Pons <98814765+P2101@users.noreply.github.com>
2023-12-27 21:54:07 +0100

    Update function.py

--- function.py
+++ function.py
@@ -45,15 +45,26 @@ while run:
         elif event.type == pygame.KEYDOWN:
             if input_box_active:
                 if event.key == pygame.K_RETURN:
-                    if input_text:
+                    if suggestions:
+                        # Establece el nombre de la sugerencia seleccionada como el nombre del jugador actual
+                        input_text = suggestions[selected_suggestion]
+                        input_box_active = False
+                        suggestions = []  # Limpia la lista de sugerencias
+                    elif input_text:
                         # Establece el nombre ingresado como el nombre del jugador actual
                         current_player = input_text
                         input_text = ""
                         input_box_active = False
                 elif event.key == pygame.K_BACKSPACE:
                     input_text = input_text[:-1]
+                elif event.key == pygame.K_DOWN:
+                    selected_suggestion = (selected_suggestion + 1) % len(suggestions)
+                elif event.key == pygame.K_UP:
+                    selected_suggestion = (selected_suggestion - 1) % len(suggestions)
                 else:
                     input_text += event.unicode
+                    suggestions = [nombre for nombre in names if nombre.lower().startswith(input_text.lower())]
+                    selected_suggestion = 0  # Reinicia la selección de sugerencias al escribir
         elif not input_box_active and event.type == pygame.MOUSEBUTTONDOWN:
             mouseX, mouseY = event.pos
             clicked_row = mouseY // 133
@@ -63,9 +74,10 @@ while run:
                 board_state[clicked_row][clicked_col] = current_player
                 # Cambiar al siguiente jugador
                 current_player = 'X' if current_player == 'O' else 'O'
-
-                # Volver a activar el cuadro de entrada para el próximo turno
+                # Reiniciar el cuadro de entrada para el próximo turno
+                input_text = ""
                 input_box_active = True
+                suggestions = []  # Limpia la lista de sugerencias
 
     # Dibuja el tablero
     screen.fill(white)
@@ -82,7 +94,17 @@ while run:
     width = max(200, text_surface.get_width() + 10)
     input_box_rect.w = width
     screen.blit(text_surface, (input_box_rect.x + 5, input_box_rect.y + 5))
-    
+
+    # Dibuja el menú desplegable de sugerencias
+    if suggestions:
+        for i, suggestion in enumerate(suggestions):
+            pygame.draw.rect(screen, white, (50, 430 + i * 30, 200, 30))
+            if i == selected_suggestion:
+                # Destaca la sugerencia seleccionada
+                pygame.draw.rect(screen, (200, 200, 200), (50, 430 + i * 30, 200, 30))
+            text_surface = font.render(suggestion, True, black)
+            screen.blit(text_surface, (60, 440 + i * 30))
+
     pygame.display.flip()
 
 pygame.quit()
