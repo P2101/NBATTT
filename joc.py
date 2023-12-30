d80797739d8c43ca24b0c3d9a19abc1b82d16f04
Pau Pons <98814765+P2101@users.noreply.github.com>
2023-12-30 13:05:42 +0100

    Update joc.py

--- joc.py
+++ joc.py
@@ -1,17 +1,18 @@
-import sys
 import pygame
+import sys
 import pandas as pd
 import random
 
-df = pd.read_csv('info_players.csv', sep=';')
-df
+df = pd.read_csv('all_seasons.csv')
+names = set(df['player_name'])
 
 equipos = ['ATL', 'BKN', 'BOS', 'CHA', 'CHI', 'CLE', 'DAL', 'DEN', 'DET', 'GSW', 'HOU', 'IND', 'LAC', 'LAL', 'MEM', 'MIA', 'MIL', 
  'MIN', 'NOP', 'NYK', 'OKC', 'ORL', 'PHI', 'PHX', 'POR', 'SAC', 'SAS', 'TOR', 'UTA', 'WAS']
+names = set(df['player_name'])
 
 first_r = []
 second_r = []   
-rands = random.sample(range(30), 6) # if i not in rands SI FAIG ASO DARRERA, ME TORNA UN DE BUIT, FER FUNCIÓ
+rands = random.sample(range(30), 6)
 count = 0
 
 for i in rands:
@@ -20,149 +21,172 @@ for i in rands:
         first_r.append(equipos[i])
     else:
         second_r.append(equipos[i])
+
 pygame.init()
 
-size = 700, 750
+# Game Configuration
+size = 600, 760
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption('NBA Tic Tac Toe')
+pygame.display.set_caption('Tic Tac Toe')
 
+# Colors
 white = 255, 255, 255
+black = 0, 0, 0
 
-# Define los colores de los jugadores
-PLAYER_X_COLOR = (255, 0, 0)
-PLAYER_O_COLOR = (0, 0, 255)
-FONT_COLOR = (10, 10, 10)
+# Font
+font = pygame.font.Font(None, 36)
 
-# Fuente para mostrar el ganador
-font = pygame.font.Font(None, 46)
-
-# Inicializa una matriz para representar el tablero 4x4
-board_state = [[' ', first_r[0], first_r[1], first_r[2]],
-               [second_r[0], None, None, None],
-               [second_r[1], None, None, None],
-               [second_r[2], None, None, None]]
-
-current_player = "X"  # Jugador actual, empieza con "X"
-game_over = False
-winner = None
-
-# Función para verificar si hay un ganador
-def check_winner(board_state):
-    # Verifica filas
-    for row in range(1, len(board_state)):
-        if all(board_state[row][col] == current_player for col in range(1, len(board_state))):
-            return current_player
-
-    # Verifica columnas
-    for col in range(4):
-        if all(board_state[row][col] == current_player for row in range(1, len(board_state))):
-            return current_player
-
-    # Verifica diagonales
-    if all(board_state[i][i] == current_player for i in range(1, len(board_state))) or all(board_state[i][len(board_state) - i] == current_player for i in range(1, len(board_state))):
-        return current_player
-
-    return None
-
-# Función para dibujar el tablero
-def draw_board():
-    line_width = 15
-    line_color = (0, 0, 0)  # Color negro
-
-    # Dibuja un fondo blanco
-    screen.fill(white)
-
-    # Dibuja una línea de título sobre el juego
-    pygame.draw.rect(screen, (0, 0, 0), (0, 0, size[0], 20))
-
-    # Dibuja las líneas horizontales y verticales del tablero
-    for i in range(1, 5):
-        # Líneas horizontales
-        pygame.draw.line(screen, line_color, (0, i * size[1] // 4), (size[0], i * size[1] // 4), line_width)
-        # Líneas verticales
-        pygame.draw.line(screen, line_color, (i * size[0] // 4, 40), (i * size[0] // 4, size[1]), line_width)
-
-    # Dibuja los valores en cada casilla
-    for row in range(len(board_state)):
-        for col in range(4):
-            value = board_state[row][col]
-            text = font.render(value, True, FONT_COLOR)
-            text_rect = text.get_rect(center=((col * size[0] // 4) + size[0] // 8, (row * size[1] // 4) + 20 + size[1] // 8))
-            screen.blit(text, text_rect)
-
-# Función para dibujar el cuadro de entrada
-def draw_input_box():
-    pygame.draw.rect(screen, (0, 0, 0), (0, size[1] - 50, size[0], 50))
-    pygame.draw.rect(screen, white, (10, size[1] - 40, size[0] - 20, 30))
-    
-draw = False
-run = True
+# Input Box
 input_text = ""
-input_box_active = False
+input_box_active = True
+input_box_rect = pygame.Rect(50, 650, 500, 30)
+
+# Tic Tac Toe Board (4x4)
+board_state = [[' ', '', '', ''],
+               ['', '', '', ''],
+               ['', '', '', ''],
+               ['', '', '', '']]
+
+def load_and_resize_image(img_path, target_size):
+    image = pygame.image.load(img_path)
+    return pygame.transform.scale(image, target_size)
+
+def draw_image(image, rect):
+    if image:
+        screen.blit(image, rect)
+
+for i in range(3):
+    # Load and resize images for first_r[0], first_r[1], first_r[2]
+    img_path = f'teams/{first_r[i]}.png'
+    board_state[0][i + 1] = load_and_resize_image(img_path, (140, 140))
+
+for i in range(3):
+    # Load and resize images for second_r[0], second_r[1], second_r[2]
+    img_path = f'teams/{second_r[i]}.png'
+    board_state[i + 1][0] = load_and_resize_image(img_path, (140, 140))
+
+# Current Player
+current_player = 'X'
+
+# Suggestions
+suggestions = []
+selected_suggestion = 0
+
+# Function to check for a winner or draw
+def check_winner():
+    winner = None
+
+    # Check rows and columns (excluding first row and column)
+    for i in range(1, 4):
+        if all(board_state[i][j] == current_player for j in range(1, 4)):
+            winner = current_player
+        if all(board_state[j][i] == current_player for j in range(1, 4)):
+            winner = current_player
+
+    # Check diagonals (excluding first row and column)
+    if all(board_state[i][i] == current_player for i in range(1, 4)):
+        winner = current_player
+    if all(board_state[i][4 - i] == current_player for i in range(1, 4)):
+        winner = current_player
+
+    return winner
+
+def check_player(name):
+    teams = set(df[df['player_name'] == name]['team_abbreviation'])
+    print(teams)
+    return teams
+
+# Main Game Loop
+run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
-        if not game_over and event.type == pygame.MOUSEBUTTONDOWN:
-            mouseX, mouseY = pygame.mouse.get_pos()
-            clicked_row = (mouseY - 40) // (size[1] // 4)
-            clicked_col = mouseX // (size[0] // 4)
-
-            if board_state[clicked_row][clicked_col] is None:
-                board_state[clicked_row][clicked_col] = current_player
-
-                winner = check_winner(board_state)
-                if not winner and all(all(cell is not None for cell in row) for row in board_state[1:]):
-                    draw = True
-                elif winner:
-                    game_over = True
+        elif event.type == pygame.KEYDOWN:
+            if input_box_active:
+                if event.key == pygame.K_RETURN:
+                    if suggestions:
+                        input_text = suggestions[selected_suggestion]
+                        input_box_active = False
+                        suggestions = []  # Clear suggestions list
+                    elif input_text:
+                        current_player = input_text
+                        input_text = ""
+                        input_box_active = False
+                elif event.key == pygame.K_BACKSPACE:
+                    input_text = input_text[:-1]
+                elif event.key == pygame.K_DOWN:
+                    selected_suggestion = (selected_suggestion + 1) % len(suggestions)
+                elif event.key == pygame.K_UP:
+                    selected_suggestion = (selected_suggestion - 1) % len(suggestions)
                 else:
-                    current_player = "O" if current_player == "X" else "X"
+                    input_text += event.unicode
+                    suggestions = [nombre for nombre in names if nombre.lower().startswith(input_text.lower())]
+                    selected_suggestion = 0  # Reset suggestion selection when typing
+        elif not input_box_active and event.type == pygame.MOUSEBUTTONDOWN:
+            mouseX, mouseY = event.pos
+            clicked_row = mouseY // 150
+            clicked_col = mouseX // 150
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN and input_box_active:
-                # Guarda el texto ingresado y lo muestra en la casilla correspondiente
-                input_box_active = False
-                board_state[0][clicked_col] = input_text
-                input_text = ""
-            elif event.key == pygame.K_BACKSPACE:
-                input_text = input_text[:-1]
-            elif input_box_active:
-                input_text += event.unicode
+            # Ignore clicks in the first row and first column
+            if 0 < clicked_row < 4 and 0 < clicked_col < 4 and board_state[clicked_row][clicked_col] == '':
+                # Obtener los nombres de los equipos en las casillas correspondientes
+                team1 = first_r[clicked_col-1]
+                team2 = second_r[clicked_row-1]
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            # Verifica si se hizo clic en el cuadro de entrada
-            if (10 <= event.pos[0] <= size[0] - 10) and (size[1] - 40 <= event.pos[1] <= size[1] - 10):
-                input_box_active = not input_box_active
+                print(f"Team 1: {team1}")
+                print(f"Team 2: {team2}")
+                print(f"Input Text: {input_text}")
 
-    # Dibuja el tablero en la pantalla
-    draw_board()
+                # Verificar si el jugador seleccionado está entre los dos equipos
+                player_teams = check_player(input_text)
+                if team1 in player_teams and team2 in player_teams:
+                    print("Valid move!")
+                    board_state[clicked_row][clicked_col] = current_player
+                    winner = check_winner()
+                    if winner:
+                        print(f"Player {winner} wins!")
+                        run = False
+                    elif all(all(cell != '' for cell in row[1:]) for row in board_state[1:]):
+                        print("It's a draw!")
+                        run = False
 
-    # Dibuja el cuadro de entrada
-    draw_input_box()
-    if input_box_active:
-        pygame.draw.rect(screen, (0, 0, 0), (10, size[1] - 40, size[0] - 20, 30), 2)
-        font = pygame.font.Font(None, 36)
-        text_surface = font.render(input_text, True, white)
-        screen.blit(text_surface, (20, size[1] - 35))
+                    current_player = 'X' if current_player == 'O' else 'O'
+                    input_text = ""
+                    input_box_active = True
+                    suggestions = []
+                else:
+                    print("Invalid move!")
 
-    # Muestra el ganador si hay uno
-    if draw:
-        text = font.render("¡Empate!", True, FONT_COLOR)
-        text_rect = text.get_rect(center=(size[0] // 2, size[1] // 2.2))
-        screen.blit(text, text_rect)
+    # Draw the Board
+    screen.fill(white)
+    for row in range(4):
+        for col in range(4):
+            pygame.draw.rect(screen, black, (col * 150, row * 150, 150, 150), 2)
+            if isinstance(board_state[row][col], pygame.Surface):
+                draw_image(board_state[row][col], (col * 150 + 5, row * 150 + 5))
+            else:
+                text_surface = font.render(str(board_state[row][col]), True, black)
+                text_rect = text_surface.get_rect(center=(col * 150 + 75, row * 150 + 75))
+                screen.blit(text_surface, text_rect)
 
-    if game_over:
-        if winner:
-            text = font.render("¡El jugador " + winner + " ha ganado!", True, FONT_COLOR)
-        else:
-            text = font.render("¡Empate!", True, FONT_COLOR)
+    # Draw the Input Box
+    pygame.draw.rect(screen, black, input_box_rect, 2)
+    text_surface = font.render(input_text, True, black)
+    width = max(500, text_surface.get_width() + 10)
+    input_box_rect.w = width
+    screen.blit(text_surface, (input_box_rect.x + 5, input_box_rect.y + 5))
 
-        # Centra el texto en la parte superior de la pantalla
-        text_rect = text.get_rect(center=(size[0] // 2.2, 80))
-        screen.blit(text, text_rect)
+    # Draw Suggestions
+    if suggestions:
+        for i, suggestion in enumerate(suggestions):
+            pygame.draw.rect(screen, white, (50, 680 + i * 30, 500, 30))
+            if i == selected_suggestion:
+                pygame.draw.rect(screen, (200, 200, 200), (50, 680 + i * 30, 500, 30))
+            text_surface = font.render(suggestion, True, black)
+            screen.blit(text_surface, (60, 690 + i * 30))
 
     pygame.display.flip()
 
 pygame.quit()
+sys.exit()
